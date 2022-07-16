dcd705e6dae931b5d3a857effc2ad184699f649e
NUMADSp22_JoseLou <notsky@gmail.com>
2022-07-16 13:13:50 -0400

    HW8 - Firechat App v2.4 - Added login and register activities

---
+++ app/src/main/java/edu/neu/firebasechatapp/RegisterActivity.java
@@ -0,0 +1,104 @@
+package edu.neu.firebasechatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth userAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // Initialize Firebase Auth
+        userAuth = FirebaseAuth.getInstance();
+        if (userAuth.getCurrentUser() != null) {
+            finish();
+            return;
+        }
+
+//        TextView textViewSwitchToLogin = findViewById(R.id.loginTextView);
+    }
+
+    /**.
+     * Register user in firebase
+     */
+    public void registerUser() {
+        // point to input
+        EditText etName = findViewById(R.id.etNameCA);
+        EditText etUsername = findViewById(R.id.etUsernameCA);
+        EditText etPassword = findViewById(R.id.etPasswordCA);
+
+        // parse text
+        String name = etName.getText().toString();
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (name.isEmpty() || username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_LONG);
+            return;
+        }
+
+        userAuth.createUserWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            User user = new User(name, username, password);
+                            FirebaseDatabase.getInstance().getReference("users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            showMainActivity();
+                                        }
+                                    });
+                        }
+                        else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(RegisterActivity.this, "Authentication failed",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void showMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    /**.
+     * Open new activity/page - Login
+     *
+     * @param view  the view object
+     */
+    public void loginActivity(View view) {
+        // Set variables
+        int thisId = view.getId();
+
+        // If id for button is the same then go to next page/activity
+        if (thisId == R.id.tvLogin) {
+            // Set intent and start new activity
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/neu/firebasechatapp/User.java
@@ -0,0 +1,16 @@
+package edu.neu.firebasechatapp;
+
+public class User {
+    private String name;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+}
